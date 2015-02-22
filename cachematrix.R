29d16fad178a6d1057708087de2413b6903afa06
scratch-area <scratch@cs331.info>
2015-02-22 17:35:42 -0600

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,33 @@
-##Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix rather 
-##than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). 
-##Your assignment is to write a pair of functions that cache the inverse of a matrix.
-
-
-
-##makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
-
-For this assignment, assume that the matrix supplied is always invertible.
-
-## Creates a Matrix
-
 makeCacheMatrix <- function(x = matrix()) {
-        inversse <-NULL
-        
+  inverted <- NULL
+  set <- function(y) {
+    x <<- y
+    inverted <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inverted <<- inverse
+  getinverse <- function() inverted
+  list(set=set, get=get, 
+       setinverse=setinverse, 
+       getinverse=getinverse)
 }
 
 
-##cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
-##If the inverse has already been calculated (and the matrix has not changed), 
-##then the cachesolve should retrieve the inverse from the cache.
-##
-##Computing the inverse of a square matrix can be done with the solve function in R. 
-##For example, if X is a square invertible matrix, then solve(X) returns its inverse.
+# The following function returns the inverse of the matrix. It first checks if
+# the inverse has already been computed. If so, it gets the result and skips the
+# computation. If not, it computes the inverse, sets the value in the cache via
+# setinverse function.
 
-
+# This function assumes that the matrix is invertible.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  matInv <- x$getinverse()  ## try to get inverted Matrix from cache
+  if(!is.null(matInv)) {   ##is.na gives error message so switched to NULL 
+    message("getting cached data.")
+    return(matInv)
+  } 
+  data <- x$get()
+  ##Use solve to determine inverse of Matrix if inverse not calculated earlier
+  matInv <- solve(data)
+  x$setinverse(matInv)   ##store the inverse so inverse will not need to be calculated next time
+  matInv
 }
